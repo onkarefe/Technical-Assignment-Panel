c6816315d5fd6c5f03cd44ef30c2fd80adb49889
Efe Onkar <66698996+onkarefe@users.noreply.github.com>
2022-12-30 02:05:06 +0300

    Delete panel_assignment directory

--- panel_assignment/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-$baglan=new PDO("mysql:host=localhost;dbname=piworks;charset=utf8","root","");
-$baglan->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$baglan->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$baglan->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-$baglan->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
